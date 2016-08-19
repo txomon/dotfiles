6293b166b5db983c2de21d712f855e69cb5d16c0
Javier Domingo Cansino <javier@jinnapp.com>
2016-08-19 09:02:30 +0200

    Fish has all the configuration through fish_config, no need for manual configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,5 @@
 fish_vi_key_bindings
 
-source $HOME/.config/fish/solarized.fish
-
 # Load general configuration
 if test -d "$HOME/.env-vars/"
         for f in (find $HOME/.env-vars/ -name '*.env' -or -name '*.env.fish')
