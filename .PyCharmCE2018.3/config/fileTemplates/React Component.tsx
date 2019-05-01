f9952f05a71e874a72c1d5acd527685f39151d92
Javier Domingo Cansino <javierdo1@gmail.com>
2019-05-01 15:36:28 +0100

    Bring sam up to date

---
+++ .PyCharmCE2018.3/config/fileTemplates/React Component.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface ${FILE_NAME}Props {
+
+}
+
+export class ${FILE_NAME} extends React.Component<{FILE_NAME}Props, {}> {
+  render() {
+    return ;
+  }
+}
