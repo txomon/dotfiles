8ce57b88f5abb38dff8d68875d608fbbd73e876f
Javier Domingo Cansino <javierdo1@gmail.com>
2025-08-15 14:15:04 +0200

    Add new custom binaries and update ignores+starship

---
+++ bin/docker-find-image-by-overlay.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+layer_id=$1
+
+docker image ls -qa \
+  | while read -r imageid; do
+      docker image inspect --format '{{json .GraphDriver.Data}}' "$imageid" \
+        | grep -q "$layer_id" && echo "$imageid"
+    done
